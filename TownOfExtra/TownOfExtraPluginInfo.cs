7db478129138e2c537d089a0a96624b6971d6f97
Mehzxzz <243104136+Mehzxzz@users.noreply.github.com>
2026-04-27 18:59:28 +0100

    fixed version tag

--- TownOfExtra/TownOfExtraPluginInfo.cs
+++ TownOfExtra/TownOfExtraPluginInfo.cs
@@ -4,5 +4,5 @@ public class TownOfExtraPluginInfo
 {
     public const string Id = "me.mehzxzz.townOfExtra";
     public const string Name = "TownOfExtra";
-    public const string Version = "0.9.1";
+    public const string Version = "0.9.2";
 }
