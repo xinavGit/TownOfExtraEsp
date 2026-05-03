b6b7d158e1bd13f478313a7c8f2985d3efb692fa
Mehzxzz <243104136+Mehzxzz@users.noreply.github.com>
2026-05-03 22:55:51 +0100

    upd ver

--- TownOfExtra/TownOfExtraPluginInfo.cs
+++ TownOfExtra/TownOfExtraPluginInfo.cs
@@ -4,6 +4,6 @@ public class TownOfExtraPluginInfo
 {
     public const string Id = "me.mehzxzz.townOfExtra";
     public const string Name = "Town Of Extra";
-    public const string Version = "1.0.3-dev";
+    public const string Version = "1.0.3";
     public const bool IsPreRelease = true;
 }
