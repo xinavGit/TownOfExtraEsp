1d650ac1e98ea3bfcedc7aae69c13a83421e826d
espeon3 <espeonicalic2@gmail.com>
2026-07-09 16:58:21 +0200

    SerialKiller base

---
+++ TownOfExtra/Roles/Impostor/Killing/SerialKillerRole.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using Il2CppInterop.Runtime.Attributes;
+using MiraAPI.Roles;
+using TownOfUs.Extensions;
+using TownOfUs.Modules.Wiki;
+using TownOfUs.Roles;
+using TownOfUs.Utilities;
+using UnityEngine;
+
+namespace TownOfExtra.Roles.Impostor.Killing;
+
+public sealed class SerialKillerRole: ImpostorRole, ITownOfUsRole, IWikiDiscoverable, IDoomable, ICrewVariant, IUnlovable
+{
+    public string Rolename => "Serial Killer";
+    public string RoleDescription => "Unleash your bloodlust on others!";
+    public string RoleLongDescription => RoleDescription;
+    public color RoleColor => palette.ImpostorRed;
+    public ModdedRoleTeams Team => ModdedRoleTeams.Impostor;
+    public DoomableType DoomHintType => DoomableType.Fearmonger;
+    public RoleBehaviour CrewVariant => RoleManager.Instance.GetRole((RoleTypes)RoleId.Get<SheriffRole>());
+
+public string GetAdvancedDescription()
+{
+    return
+          "The Serial Killer is an Impostor Killing role that has a lower kill cooldown than others."  
+
+}
+}
+
